fe972f76e664e336124c89c0f5e73306c2366301
misterD05 <157386570+misterD05@users.noreply.github.com>
2024-02-28 09:04:54 +0100

    Add files via upload

---
+++ nursej.js
@@ -0,0 +1,6 @@
+var onHome = false;
+
+function changeColor(){
+    document.getElementsById("menuOp").style.rgb = (185,0,0);
+}
+var onhomeIcon = document.getElementsByClassName("menuOp")[0].onclick = function(){changeColor};
