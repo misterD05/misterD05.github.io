b1beed0b8010cba344cb56b1b4c14c6d8d24b09b
misterD05 <157386570+misterD05@users.noreply.github.com>
2024-03-03 14:56:15 +0100

    Update nursej.js

--- nursej.js
+++ nursej.js
@@ -18,68 +18,43 @@ function openMenu(){
     }
 }
 
-var ID = null;
-
-function animation(element, posStarter){
-
-    clearInterval(ID);
-    ID = setInterval(frame, 40);
-    var finish = false;
-    function frame(){
-        if(finish){
-            clearInterval(ID);
-        }else{
-            for (let index = 1; index < 12; index++) {
-                posStarter[index] = posStarter[index] + 2;
-                element[index].style.bottom = posStarter[index] + "px";
-            }
-         }
-    }
-}
-myWallpaper();
 
 
-function getRandomIntInclusive(min, max) {
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
-  }
 
-function graphicSfondoSet(){
-    var b = "";
+function bubblesUp(){
     var ball = null;
+    var index = 0;
+    var numOFballs = window.innerWidth / 10;
     var measure = 0;
     var pos2 = 0;
     var pos3 = 0;
-    const posStart =[];
-    for (let index = 1; index < 13; index++) {
-        b = 'b' + index;
-        ball = document.getElementById(b);
-        measure = getRandomIntInclusive(10, 40);
-        pos2 = getRandomIntInclusive(0,100);
-        pos3 = getRandomIntInclusive(-800, 0);
+    var father = document.getElementById("containAnimations");
+
+    while(index < numOFballs) {
+        bid = 'b' + index;
+        ball = document.createElement("div");
+        ball.classList.add("ball");
+
+        measure = Math.random() * 50;
+        pos2 = Math.floor(Math.random() * window.innerWidth);
+        pos3 = Math.floor(Math.random() * window.innerHeight);
+        
+        ball.style.left =  pos2 / 6 + "%";
+        ball.style.bottom = pos3 / 6 + "%";
+
         ball.style.height = measure + 'px';
         ball.style.width = measure + 'px';
-        ball.style.left = pos2 + "%"
-        ball.style.bottom = pos3 + "px";
-        posStart[index] = pos3;
-    }
 
-    return posStart;
-}
-
-
-function myWallpaper(){
-        var posStarter = graphicSfondoSet();
-        var b = "";
-        var ball = null;
-        const balls = [];
-        for (let index = 1; index < 13; index++) {
-            b = "b" + index;
-            ball = document.getElementById(b);
-            balls[index] = ball;
-
-        }
-        animation(balls, posStarter);
+        ball.style.animationDuration = 1.5 *measure + "s";
         
+        father.appendChild(ball);
+        
+        index++
+    }
+    
 }
+
+bubblesUp();
+
+
+
