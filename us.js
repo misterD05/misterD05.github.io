d899bdab15687318aaed3362c568b74583dc09e8
misterD05 <157386570+misterD05@users.noreply.github.com>
2024-03-19 23:30:28 +0100

    Update us.js

--- us.js
+++ us.js
@@ -6,9 +6,12 @@ function spostaText(){
         text.style.top = "85%"
         text.style.left = "2%"
         text.style.width = "96%"
+        document.getElementById("video2").style.top = "150%"
+        document.getElementById("title2").style.top = "130%"
     }else{
-        text.style.left = "55%"
-        text.style.width = "42%"
+        text.style.left = "58%"
+        text.style.width = "40%"
+
     }
 
 }
