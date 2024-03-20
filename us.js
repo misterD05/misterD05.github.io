bd81ca600ddd1f672a1a1416836721710f96c563
misterD05 <157386570+misterD05@users.noreply.github.com>
2024-03-20 18:28:28 +0100

    Update us.js

--- us.js
+++ us.js
@@ -3,11 +3,14 @@ function spostaText(){
     var size = text.getBoundingClientRect(text)
 
     if(size.width <= "400"){
-        text.style.top = "85%"
+        text.style.top = "95%"
         text.style.left = "2%"
         text.style.width = "96%"
         document.getElementById("video2").style.top = "150%"
         document.getElementById("title2").style.top = "130%"
+        document.getElementById("documentRel").style.top = "210%"
+        document.getElementById("documentRel").style.left = "2%"
+    
     }else{
         text.style.left = "58%"
         text.style.width = "40%"
